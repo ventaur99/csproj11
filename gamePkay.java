761864b61aadd88a07039a32b3102e1df034697d
ventaur99 <venicediente@gmail.com>
2025-03-10 20:37:10 +0800

    Update gamePkay.java

--- gamePkay.java
+++ gamePkay.java
@@ -6,15 +6,17 @@ package csproj;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class gamePkay extends JFrame {
     public gamePkay() {
-        setTitle("GODS/GODESSES");
-        setSize(1000, 700);
+        setTitle("MGA DIYOS AT DIYOSA");
+        setSize(1100, 700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-
-        add(new GameScreen());
+        
+        add(new GamePanel()); 
 
         setVisible(true);
     }
@@ -24,17 +26,57 @@ public class gamePkay extends JFrame {
     }
 }
 
-class GameScreen extends JPanel {
-    private Image backgroundImage;
+class GamePanel extends JPanel {
+    private Image brickImage;
+    private JButton redButton, yellowButton, orangeButton, greenButton, blueButton, pinkButton;
 
-    public GameScreen() {
-        backgroundImage = new ImageIcon(getClass().getResource("/images/brick.png")).getImage();
-        System.out.println(getClass().getResource("/csproj/images/brick.png"));
+    public GamePanel() {
+        setLayout(null);
+
+        
+        brickImage = new ImageIcon(getClass().getResource("/images/brick.png")).getImage();
+
+        
+        redButton = createButton("/images/red.png", 250, 150);
+        yellowButton = createButton("/images/yellow.png", 450, 150);
+        orangeButton = createButton("/images/orange.png", 650, 150);
+        greenButton = createButton("/images/green.png", 300, 350);
+        blueButton = createButton("/images/blue.png", 600, 350);
+        pinkButton = createButton("/images/pink.png", 450, 500);
+
+        
+        add(redButton);
+        add(yellowButton);
+        add(orangeButton);
+        add(greenButton);
+        add(blueButton);
+        add(pinkButton);
+    }
+
+    private JButton createButton(String imagePath, int x, int y) {
+        ImageIcon icon = new ImageIcon(getClass().getResource(imagePath)); 
+        JButton button = new JButton(icon);
+        button.setBounds(x, y, icon.getIconWidth(), icon.getIconHeight());
+        button.setBorderPainted(false);
+        button.setContentAreaFilled(false);
+        button.setFocusPainted(false);
+        button.addActionListener(new ButtonClickListener());
+        return button;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g); 
-        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        super.paintComponent(g);
+        g.drawImage(brickImage, 0, 0, getWidth(), getHeight(), this);
     }
 }
+
+
+class ButtonClickListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton source = (JButton) e.getSource();
+        JOptionPane.showMessageDialog(null, "You selected: " + source.getIcon().toString());
+    }
+}
+
